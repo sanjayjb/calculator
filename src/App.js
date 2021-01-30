0fd35053ed8da9e4577678ff2bffa11dc2cf4a8c
WHQ_NT_DOMAIN\SB066850 <sanjay.bharadwaj@cerner.com>
2021-01-31 00:52:00 +0530

    addition of light mode and dark mode

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
   const [input, setInput] = useState("");
   const [operators, setOperators] = useState([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "."]);
   const [scientificCalc, setScientificCalc] = useState(false);
+  const [calcBackgroundMode, setCalcBackgroundMode] = useState(false);
   const [displayScientificOptions, setDisplayScientificOptions] = useState("none");
   const calcBtns = [];
+ 
   operators.forEach((item) => {
     calcBtns.push(
       <button
@@ -27,11 +29,37 @@ function App() {
      {
        setScientificCalc(false);
        setDisplayScientificOptions("none");
+       
      }
      else
      {
        setScientificCalc(true);
        setDisplayScientificOptions("block");
+     }
+  }
+  function toggleCalcBackgroundMode()
+  {
+     let x = document.querySelectorAll("button");
+     if(calcBackgroundMode ==  true)
+     {
+       setCalcBackgroundMode(false);
+       document.body.style.backgroundColor = "#fff";
+       document.getElementById("inputText").style.color = "#000"
+       x.forEach((item) => {
+         item.style.color = "#000"
+         item.style.backgroundColor = "#f0f0f0"
+       })
+       
+     }
+     else
+     {
+       setCalcBackgroundMode(true);
+       document.body.style.backgroundColor = "#000";
+       document.getElementById("inputText").style.color = "#fff"
+       x.forEach((item) => {
+        item.style.color = "#fff"
+        item.style.backgroundColor = "#666"
+      })
      }
   }
 
@@ -40,7 +68,10 @@ function App() {
       <button onClick = {toggleCalcMode.bind(this)} >
         {scientificCalc ? "Normal Mode" : "Scientific mode"}
       </button>
-      <div className="show-input">{input}</div>
+      <button onClick = {toggleCalcBackgroundMode.bind(this)} >
+        {calcBackgroundMode ? "Normal Theme" : "Dark Theme"}
+      </button>
+      <div id="inputText" className={`show-input`} >{input}</div>
       <div className="digits flex">{calcBtns}</div>
       <div className="modifiers subgrid">
         <button onClick={() => setInput(input.substr(0, input.length - 1))}>
